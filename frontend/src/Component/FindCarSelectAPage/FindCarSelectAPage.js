14c2d8ff63da8632bfd1c807374c97e4451ff0ba
JYJIAN <e0117e0117@gmail.com>
2023-02-10 21:00:05 +0800

    fix FindCarSelectA bug

--- frontend/src/Component/FindCarSelectAPage/FindCarSelectAPage.js
+++ frontend/src/Component/FindCarSelectAPage/FindCarSelectAPage.js
@@ -11,31 +11,35 @@ function FindCarSelectAPage() {
   useEffect(() => {
 		// Using fetch to fetch the api from
 		// flask server it will be redirected to proxy
-    
+
 		fetch("/A_parking_spaces").then((res) =>
 			res.json().then((data) => {
 				// Setting a data from api
 				setdata({
 					A_parking_lot: data.A
-				});
+				})
 			})
-		);
+    );
 	}, []);
-/*
-console.log(data.A_parking_lot[0]);
 
+  if(data.A_parking_lot == false){
+    return (<div>Loading...</div>);
+  }
+  console.log(data.A_parking_lot[99]);
+  
   var empty_spaces = 0;
-  for(var i = 0; i < 100; i++){
-    if(data.A_parking_lot[i]["is_empty"])
+  for(let i = 0; i < 100; i++){
+    if(data.A_parking_lot != null && data.A_parking_lot[i]["is_empty"] == true)
       empty_spaces += 1;
   }
-  */
+
   return (
     <div>
         <h1>A 區的車位剩餘{empty_spaces}</h1>
+        <h1>A-{data.A_parking_lot[0]["id"]} 車位狀態: {data.A_parking_lot[0]["is_empty"]}</h1>
     </div>
-  );
-  
+  );  
+
 }
 
 export default FindCarSelectAPage;
--- frontend/src/setupProxy.js
+++ frontend/src/setupProxy.js
@@ -2,7 +2,8 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 
 module.exports = function(app) {
   app.use(
-    ['/parking_remain_spaces', '/data', "/A_parking_spaces"],
+    ['/parking_remain_spaces', '/data',
+      "/A_parking_spaces", "/B_parking_spaces", "/C_parking_spaces", "/D_parking_spaces"],
     createProxyMiddleware({
       target: 'http://127.0.0.1:8000',
       changeOrigin: true,
