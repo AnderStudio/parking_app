44f4943a42518b8767d4cb22f9b7673e4f3ae708
AnderStudio <chouyuhung1@gmail.com>
2023-02-11 00:44:34 +0800

    find my car page  done

--- frontend/src/Component/FindCarSelectionPage/FindCarSelectionPage.js
+++ frontend/src/Component/FindCarSelectionPage/FindCarSelectionPage.js
@@ -8,9 +8,16 @@ class FindCarPage extends Component {
     return (
       <div>
         <h1>Find My Car</h1>
-        <div className="input-group mb-3">
-          <input type="text" className="form-control" placeholder="License plate number" aria-label="Recipient's username" aria-describedby="button-addon2" />
+        <div className="input-group mb-3 d-flex flex-column">
+          <div className="d-flex flex-row justify-content-center">
+            <input type="text" className="form-control w-50" placeholder="License plate number" aria-label="Recipient's username" aria-describedby="button-addon2" />
             <button className="btn btn-outline-secondary" type="button" id="button-addon2">Button</button>
+          </div>
+          <div className="d-flex flex-column justify-content-center vh-50">
+            <h3>停車場：C</h3>
+            <h3>車位：009</h3>
+            <h3>已停放時間：00：50:00</h3>
+          </div>
         </div>
       </div>
     );
