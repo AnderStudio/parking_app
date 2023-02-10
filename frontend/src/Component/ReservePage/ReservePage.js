dff68dc7c2d70199ff8f8ca70e09d40528a19dd9
JYJIAN <e0117e0117@gmail.com>
2023-02-10 13:34:36 +0800

    reserve modal ok

--- frontend/src/Component/ReservePage/ReservePage.js
+++ frontend/src/Component/ReservePage/ReservePage.js
@@ -19,6 +19,33 @@ class ReservePage extends Component {
             <a href="#" className="btn btn-primary">Delete</a>
           </div>
         </div>
+
+
+        <button type="button" className="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
+          Launch demo modal
+        </button>
+
+
+        <div className="modal fade" id="exampleModal" tabIndex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+          <div className="modal-dialog">
+            <div className="modal-content">
+              <div className="modal-header">
+                <h1 className="modal-title fs-5" id="exampleModalLabel">Modal title</h1>
+                <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+              </div>
+              <div className="modal-body">
+                <p>ok</p>
+              </div>
+              <div className="modal-footer">
+                <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                <button type="button" className="btn btn-primary">Save changes</button>
+              </div>
+            </div>
+          </div>
+        </div>
+
+
+
         <nav className="d-flex flex-column">
           <Link to="/ReserveBook">
             <button className="e-button" type="primary">新增預約</button>
